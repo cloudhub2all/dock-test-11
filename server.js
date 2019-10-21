2e79a9530a1f37877f00152b4577209382a9fd30
cloudhub2all <55729309+cloudhub2all@users.noreply.github.com>
2019-10-21 18:24:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world- Automation Tested 1001 ');
+  res.send('Hello world- Automation Tested 10022 ');
 });
 
 app.listen(PORT, HOST);
